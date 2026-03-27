5902c9a9c82e7d110d461a4f1867a12726bc7cd1
Sakthivel Vijayakumar <196232492+sakthivelvjkumar@users.noreply.github.com>
2026-03-27 14:26:16 -0400

    flask backend

---
+++ app.py
@@ -0,0 +1,28 @@
+import flask
+import db
+
+app = flask.Flask(__name__)
+
+
+# Serve frontend
+@app.route("/")
+@app.route("/index")
+def index():
+    return flask.render_template("index.html")
+
+
+# API endpoint
+@app.route("/api/artworks")
+def artworks():
+    data = db.get_all_artworks()
+    return flask.jsonify(data)
+
+@app.route("/api/artworks/nearby")
+def nearby():
+    lat = float(flask.request.args.get("lat"))
+    lon = float(flask.request.args.get("lon"))
+    return flask.jsonify(db.get_nearby_artworks(lat, lon))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

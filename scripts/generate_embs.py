2c28571a0642f4bd955935b7396a691406ada73a
Arnav Ambre <arnav.ambre@princeton.edu>
2026-04-23 23:16:43 -0400

    moving to smaller dino model, updated embeddings, reformatted all python files using black

--- scripts/generate_embs.py
+++ scripts/generate_embs.py
@@ -5,22 +5,37 @@ from PIL import Image
 from io import BytesIO
 import torch
 import clip
+from torchvision import transforms
+import torch.nn.functional as F
 
 DEVICE = torch.device("cpu")
+DTYPE = torch.float16
 
-model, preprocess = clip.load("RN50", device=DEVICE)
+model = torch.hub.load("facebookresearch/dinov2", "dinov2_vits14")
+model.eval()
+model = model.to(DEVICE).to(DTYPE)
 
-model.half()
+preprocess = transforms.Compose(
+    [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=(0.485, 0.456, 0.406),
+            std=(0.229, 0.224, 0.225),
+        ),
+    ]
+)
 
 OUTPUT_DIR = "embeddings"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 
 def get_embedding_from_pil(image):
-    image_input = preprocess(image).unsqueeze(0).to(DEVICE)
+    image_input = preprocess(image).unsqueeze(0).to(DEVICE).to(DTYPE)
     with torch.no_grad():
-        embedding = model.encode_image(image_input)
-    return embedding / embedding.norm(dim=-1, keepdim=True)
+        embedding = model(image_input)
+    return F.normalize(embedding, dim=-1)
 
 
 def download_image(url):
